58897afafe7ade49f52182015a243ca0060ad0b5
nallari <shruti.nallari@gmail.com>
2025-07-15 21:17:33 -0400

    Dynamic routes made super easy with [productId] for productId to any unique

---
+++ sai-next/src/app/products/page.tsx
@@ -0,0 +1,12 @@
+export default function Page(){
+    return(
+        <>
+        <h1>Products</h1>
+        <ul>
+            <li>Products 1</li>
+            <li>Products 2</li>
+            <li>Products 3</li>
+        </ul>
+        </>
+    )
+}
